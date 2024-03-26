21615da88c4f73f97005303ad43c48fe5cb15cf4
luisFPM97 <134348310+luisFPM97@users.noreply.github.com>
2024-03-26 14:49:41 -0500

    Update index.js

--- src/models/index.js
+++ src/models/index.js
@@ -5,14 +5,4 @@ const Producto = require("./Producto");
 const Subcategoria = require("./Subcategoria");
 const Descript = require("./Descript");
 
-Empresa.hasMany(Categoria);
-Categoria.belongsTo(Empresa);
 
-Categoria.hasMany(Producto);
-Producto.belongsTo(Categoria);
-
-Producto.hasMany(Subcategoria);
-Subcategoria.belongsTo(Producto);
-
-Subcategoria.hasMany(Descript);
-Descript.belongsTo(Subcategoria);
